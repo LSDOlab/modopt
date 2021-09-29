0dc25c1dcb0001f56bb36df5518c0c92f891e99f
Anugrah Jo Joshy <ajoshy@eng.ucsd.edu>
2021-09-28 21:28:01 -0700

    Deleted unnecessary files from docs folder

--- docs/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/anugrah/Packages/modopt/docs/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/anugrah/Packages/modopt/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/anugrah/Packages/modopt/docs/node_modules/@docusaurus/theme-classic/lib/admonitions.css"),
-  require("/Users/anugrah/Packages/modopt/docs/src/css/custom.css"),
-];
