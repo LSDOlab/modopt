d395b8c8c71267c5ca3bc33c7e47c43b00599958
anugrahjo <ajoshy.ucsd@gmail.com>
2024-08-01 14:33:28 -0700

    Update Cantilever beam problems - add __main__, use ldl solver in CasADi, rename problem.

--- examples/ex_15_3cantilever_beam_csdl.py
+++ examples/ex_15_3cantilever_beam_csdl.py
@@ -57,47 +57,48 @@ def get_problem(n_el): # 23 statements excluding comments, returns, name assignm
     sim = csdl.experimental.JaxSimulator(rec)
 
     # Instantiate your problem using the csdl Simulator object and name your problem
-    prob = CSDLAlphaProblem(problem_name=f'Cantilever beam {n_el} elements CSDL', simulator=sim)
+    prob = CSDLAlphaProblem(problem_name=f'cantilever_{n_el}_csdl', simulator=sim)
 
     return prob
 
-# f = get_problem(50)._compute_objective(np.ones(50))
-# print('f:', f)
-# c = get_problem(50)._compute_constraints(np.ones(50))
-# print('c:', c)
+if __name__ == '__main__':
+    # # Test to see if the problem is correctly defined
+    # prob = get_problem(50)
+    # print(prob._compute_objective(np.ones(50)))       # 39.99999999905752
+    # print(prob._compute_constraints(np.ones(50)))     # [0.09]
+    # exit()
 
-# SLSQP
-print('\tSLSQP \n\t-----')
-n_el = 50
-optimizer = SLSQP(get_problem(n_el), solver_options={'maxiter': 200, 'ftol': 1e-9})
-optimizer.check_first_derivatives(np.ones(50))
-start_time = time.time()
-optimizer.solve()
-opt_time = time.time() - start_time
-success = optimizer.results['success']
+    # SLSQP
+    print('\tSLSQP \n\t-----')
+    n_el = 50
+    optimizer = SLSQP(get_problem(n_el), solver_options={'maxiter': 200, 'ftol': 1e-9})
+    start_time = time.time()
+    optimizer.solve()
+    opt_time = time.time() - start_time
+    success = optimizer.results['success']
 
-print('\tTime:', opt_time)
-print('\tSuccess:', success)
-print('\tOptimized vars:', optimizer.results['x'])
-print('\tOptimized obj:', optimizer.results['fun'])
+    print('\tTime:', opt_time)
+    print('\tSuccess:', success)
+    print('\tOptimized vars:', optimizer.results['x'])
+    print('\tOptimized obj:', optimizer.results['fun'])
 
-optimizer.print_results()
+    optimizer.print_results()
 
-import matplotlib.pyplot as plt
+    import matplotlib.pyplot as plt
 
-plt.figure()
-plt.plot(optimizer.results['x'])
-plt.xlabel('Lengthwise location')
-plt.ylabel('Optimized thickness')
-plt.show()
+    plt.figure()
+    plt.plot(optimizer.results['x'])
+    plt.xlabel('Lengthwise location')
+    plt.ylabel('Optimized thickness')
+    plt.show()
 
-assert np.allclose(optimizer.results['x'],
-                    [0.14915754,  0.14764328,  0.14611321,  0.14456715,  0.14300421,  0.14142417,
-                    0.13982611,  0.13820976,  0.13657406,  0.13491866,  0.13324268,  0.13154528,
-                    0.12982575,  0.12808305,  0.12631658,  0.12452477,  0.12270701,  0.12086183,
-                    0.11898809,  0.11708424,  0.11514904,  0.11318072,  0.11117762,  0.10913764,
-                    0.10705891,  0.10493903,  0.10277539,  0.10056526,  0.09830546,  0.09599246,
-                    0.09362243,  0.09119084,  0.08869265,  0.08612198,  0.08347229,  0.08073573,
-                    0.07790323,  0.07496382,  0.07190453,  0.06870925,  0.0653583,   0.06182632,
-                    0.05808044,  0.05407658,  0.04975295,  0.0450185,   0.03972912,  0.03363155,
-                    0.02620192,  0.01610863], rtol=0, atol=1e-5)
+    assert np.allclose(optimizer.results['x'],
+                        [0.14915754,  0.14764328,  0.14611321,  0.14456715,  0.14300421,  0.14142417,
+                        0.13982611,  0.13820976,  0.13657406,  0.13491866,  0.13324268,  0.13154528,
+                        0.12982575,  0.12808305,  0.12631658,  0.12452477,  0.12270701,  0.12086183,
+                        0.11898809,  0.11708424,  0.11514904,  0.11318072,  0.11117762,  0.10913764,
+                        0.10705891,  0.10493903,  0.10277539,  0.10056526,  0.09830546,  0.09599246,
+                        0.09362243,  0.09119084,  0.08869265,  0.08612198,  0.08347229,  0.08073573,
+                        0.07790323,  0.07496382,  0.07190453,  0.06870925,  0.0653583,   0.06182632,
+                        0.05808044,  0.05407658,  0.04975295,  0.0450185,   0.03972912,  0.03363155,
+                        0.02620192,  0.01610863], rtol=0, atol=1e-5)
