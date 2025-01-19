8809efda11cd90d21f0af98a5385b0c8760b62c7
anugrahjo <ajoshy.ucsd@gmail.com>
2025-01-18 16:55:41 -0800

    Add unit tests for documented merit functions

--- tests/test_modules.py
+++ tests/test_modules.py
@@ -3,6 +3,9 @@ import numpy as np
 
 
 def test_line_searches():
+    """
+    Unit tests for line search algorithms
+    """
     from modopt.line_search_algorithms import Minpack2LS, BacktrackingArmijo
 
     f = lambda x: (x[0]-0.5)**2 + x[1]**2
@@ -25,36 +28,84 @@ def test_line_searches():
     assert np.isclose(f2, 0., atol=1e-8)
     assert np.isclose(slope2, 0., atol=1e-4)
 
-# def test_merit_functions():
-#     '''
-#     Test merit function modules
-#     '''
+def test_merit_functions():
+    """
+    Unit tests for merit functions
+    """
+    from modopt.merit_functions import AugmentedLagrangianIneq
 
-#     # Import Base class to test
-#     from modopt import MeritFunction
-#     # Import modules to test
-#     from modopt.merit_functions import (
-#         L1Eq, L2Eq, LInfEq, 
-#         LagrangianEq, 
-#         ModifiedLagrangianIneq, 
-#         AugmentedLagrangianEq, 
-#         AugmentedLagrangianIneq)
+    f = lambda x: (x[0]-0.5)**2 + x[1]**2
+    g = lambda x: np.array([2*(x[0]-0.5), 2*x[1]])
+    c = lambda x: np.array([x[0] + x[1] - 1.]) # x[0] + x[1] - 1 >= 0
+    j = lambda x: np.array([[1., 1.]])
 
-#     # Import the problem to test with
-#     from examples.ex_8bean_function import BeanFunction
+    x = np.array([0., 0.])
+    l = np.array([1.])
+    s = np.array([0.5])
 
-#     prob = BeanFunction()
-#     tol = 1e-6
-#     maxiter = 200
-#     optimizer = SteepestDescent(prob, maxiter=maxiter, opt_tol=tol)
-#     optimizer.solve()
+    merit = AugmentedLagrangianIneq(f=f, g=g, c=c, j=j, nx=2, nc=1)
+    assert merit.rho == np.array([0.])
+    assert merit.cache == {}
+    assert merit.eval_count == {'f': 0, 'g': 0, 'c': 0, 'j': 0}
 
-#     # Check to make sure values are correct
-#     np.testing.assert_almost_equal(
-#         actual_val, 
-#         desired_val,
-#         decimal = 7,
-#     )
+    merit.set_rho(1.)
+    assert merit.rho == np.array([1.])
+
+    # AL = f(x) - l.T @ (c(x)-s) + merit.rho/2 * np.linalg.norm(c(x)-s)**2 
+    # 0.25 - 1.*(-1.-0.5) + 1./2 * (-1.-0.5)**2 = 2.875
+    AL_f = 2.875
+    print('AL_f', AL_f)
+
+    AL_cf = merit.compute_function(np.concatenate((x, l, s)))
+    print('AL_cf', AL_cf)
+    assert AL_cf == AL_f
+
+    AL_ef = merit.evaluate_function(x, l, s, f(x), c(x))
+    print('AL_ef', AL_ef)
+    assert AL_ef == AL_f
+
+    # Gradient of AL with respect to x, l and s
+    grad_x = g(x) - j(x).T @ l + merit.rho * j(x).T @ (c(x)-s)
+    grad_l = -(c(x)-s)
+    grad_s = l - merit.rho * (c(x)-s)
+    AL_g = np.concatenate((grad_x, grad_l, grad_s))
+    print('AL_g', AL_g)
+
+    # Finite difference gradient of AL with respect to x, l and s
+    v = np.concatenate((x, l, s))
+    h = 1e-8
+    AL_g_fd = -merit.compute_function(v) * np.ones_like(v)
+    for i in range(len(v)):         # x changes 3 times, so 3 new f/c(x) evaluations
+        e = np.zeros_like(v)
+        e[i] = h
+        AL_g_fd[i] += merit.compute_function(v+e)
+        AL_g_fd[i] /= h
+
+    print('AL_g_fd', AL_g_fd)
+    assert np.allclose(AL_g_fd, AL_g, atol=1e-6)
+
+
+    AL_cg = merit.compute_gradient(np.concatenate((x, l, s)))
+    print('AL_cg', AL_cg)
+    assert np.allclose(AL_cg, AL_g, atol=1e-14)
+
+    AL_eg = merit.evaluate_gradient(x, l, s, f(x), c(x), g(x), j(x))
+    print('AL_eg', AL_eg)
+    assert np.allclose(AL_eg, AL_g, atol=1e-14)
+
+    assert np.allclose(merit.cache['f'][0], x, atol=1e-14)
+    assert np.allclose(merit.cache['g'][0], x, atol=1e-14)
+    assert np.allclose(merit.cache['c'][0], x, atol=1e-14)
+    assert np.allclose(merit.cache['j'][0], x, atol=1e-14)
+
+    assert merit.cache['f'][1] == 0.25
+    assert np.allclose(merit.cache['g'][1], [-1., 0.], atol=1e-14)
+    assert np.allclose(merit.cache['c'][1], -1., atol=1e-14)
+    assert np.allclose(merit.cache['j'][1], [1., 1.], atol=1e-14)
+
+    assert merit.rho == np.array([1.])
+    print('merit.eval_count', merit.eval_count)
+    assert merit.eval_count == {'f': 1+2+1, 'g': 1, 'c': 1+2+1, 'j': 1}
 
 # def test_hessian_approximations():
 #     '''
@@ -113,4 +164,5 @@ def test_line_searches():
 
 if __name__ == '__main__':
     test_line_searches()
+    test_merit_functions()
     print('All tests passed!')
